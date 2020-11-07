ff08ae2f4e839127e36af68f9d4da6d48a18f5cf
amankumarm <amankumarm441@gmail.com>
2020-11-08 02:31:48 +0530

    added keyframe and removed js code

--- main.js
+++
@@ -1,6 +0,0 @@
-var outer=document.getElementsByClassName('outer')[0]
-var mobile=document.getElementsByClassName('mobile')[0]
-if( window.innerWidth <= 900 ){
-    outer.style.display='none'
-    mobile.style.display='block'
-}
