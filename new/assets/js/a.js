a1d0a3ef45dba4764d854ab103fd057312f30a1f
amankumarm <amankumarm441@gmail.com>
2021-06-10 11:01:09 +0530

    linked list implementaion for projets

--- new/assets/js/a.js
+++ new/assets/js/a.js
@@ -3,3 +3,11 @@
 // gsap.from("#name", {opacity: 0, x: -40, duration: 1,delay:1});
 // gsap.from(".desc", {opacity: 0, y: -40, duration: 1,delay:2});
 // gsap.from("#work_btn", {opacity: 0, y: -30, duration: 1,delay:2});
+
+
+
+// class node{
+//     constructor(data){
+//         this.
+//     }
+// }
